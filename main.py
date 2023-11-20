6aa6d0850c2779ea32186f501e36a582ef531ab8
Max-Eugène Mamet <max-eugene@hotmail.fr>
2023-11-20 13:46:11 +0100

    update requirements.txt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from datetime import datetime
+import pysurfline
 
 app = FastAPI()
 
@@ -9,6 +11,16 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+
+@app.get("/how-is-the-surf")
+def get_surf_sessions():
+    return pysurfline.get_spot_forecasts("5842041f4e65fad6a7708bca")
+
+
+@app.get("/how-is-the-surf/{spot_id}")
+def get_surf_sessions_by_spot_id(spot_id):
+    # Call the pysurfline function to get surf data
+    surf_data = pysurfline.get_spot_forecasts(spot_id)
+
+    # Return the processed data
+    return {"timestamp": datetime.now().isoformat(), "surf_data": surf_data}
